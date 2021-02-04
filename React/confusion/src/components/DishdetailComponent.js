d4675a39dbbb66c25b531a221b08983b65597d1b
Hyunwooo-Lee <hwlee3315@gmail.com>
2021-02-04 11:58:33 +0900

    Redux  Thunk

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
     Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 const required = (val) => val && val.length;
 const maxLength =  (len) => (val) => !(val) || (val.length <= len);
 const minLength =  (len) => (val) => (val) && (val.length >= len);
@@ -107,17 +107,17 @@ function RenderComments({comments, addComment, dishId}) {
         retComments = comments.map((comment) => {                
             var newStr = Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))
             return (                              
-                <p>
-                    <text className='ul' className="list-unstyled"></text>
-                    <div>
-                        <p tag="li">
-                            {comment.comment}{"\n"}
-                        </p>
-                        <p tag="li">
-                            -- {comment.author}, {newStr}
-                        </p>
-                    </div>
-                </p>
+                <div key={comment.id}>
+                    <p className='ul list-unstyled'></p>
+                    
+                    <p tag="li">
+                        {comment.comment}{"\n"}
+                    </p>
+                    <p tag="li">
+                        -- {comment.author}, {newStr}
+                    </p>
+                    
+                </div>
             );
         });
     }
@@ -134,49 +134,64 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 function RenderDish({dish}) {
-    if(dish != null){
-        return (
-            <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle><h3>{dish.name}</h3></CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+    return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle><h3>{dish.name}</h3></CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+const DishDetail = (props) => {
+    if(props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
             </div>
         );
     }
+    else if(props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if(props.dish != null) 
+        return (  
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} 
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />                                                     
+                </div>
+            </div>
+        );
     else {
         return (
             <div></div>
         );
-    }        
-}
-
-const DishDetail = (props) => {
-    console.log('DishDetail Component render is invoked');
-    
-    return (  
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
-                </div>
-            </div>
-            <div className="row">
-                <RenderDish dish={props.dish} />
-                <RenderComments comments={props.comments} 
-                    addComment={props.addComment}
-                    dishId={props.dish.id} />                                                     
-            </div>
-        </div>
-    );        
+    }     
 }
 
 
