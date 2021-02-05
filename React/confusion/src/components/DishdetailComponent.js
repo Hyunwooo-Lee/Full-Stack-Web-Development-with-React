3addd3d21d87bb602350864a1b98912474ad715f
Hyunwooo-Lee <hwlee3315@gmail.com>
2021-02-05 12:09:52 +0900

    Fetch From Server

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 const required = (val) => val && val.length;
 const maxLength =  (len) => (val) => !(val) || (val.length <= len);
 const minLength =  (len) => (val) => (val) && (val.length >= len);
@@ -135,7 +137,7 @@ function RenderDish({dish}) {
     return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle><h3>{dish.name}</h3></CardTitle>
                     <CardText>{dish.description}</CardText>
